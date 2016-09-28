1458da0828f27f363414c7c0d7a54b109b5a7e63
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-09-28 18:01:35 -0500

    working on flow

---
+++ ACA2016/src/edu/uca/aca2016/basics/Flow.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.basics;
+
+/**
+ *
+ * @author brela
+ */
+public class Flow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int i= 0; i<201; i+= 10) {
+            System.out.println("Count is:" + i);
+        }
+    }
+    
+}
