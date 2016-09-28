52dc9e9959147625704cc0fb77332f745da9766c
Username <Username@LAPTOP-9SOPAM02.philander.edu>
2016-09-27 20:52:06 -0500

    saving progress

---
+++ ACA2016/src/edu/uca/aca2016/basics/Team.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.basics;
+
+/**
+ *
+ * @author Username
+ */
+public class Team {
+
+    /**
+     * @param args the command line arguments
+     */ 
+    private String Name; 
+    private String Sport;
+    private String City;
+    private String State;
+    private short Number_of_players; 
+    
+    
+        public void setName(String value){ 
+        this.Name = value;
+            
+        public void setSport(String value)
+        this.Sport = value;
+     
+        public void setCity(String value) 
+        this.City = ();
+    }
+        public void setState(String value) {
+        this.State = ();
+    }
+        public void setNumber_of_players(short value) {
+        this.Number_of_players = value;
+        
+            public String getName() {
+        return this.Name;
+    }
+                }
+            public String getSport() {
+        return this.Sport;
+    }
+            public String getCity() {
+        return this.City;
+    }
+            public String getState() {
+        return this.State;
+    }
+                public short getNumber_of_players {
+        return Number_of_players;
+    }
+            
+    /**
+     * @param args the command line arguments
+     */
+    
+
+    public static void main(String[] args) {
+        Team = Name; 
+        
+        // set the team name
+        Team.setName("Rangers");
+
+        // set sport
+        Sport.setSport("Soccer")
+
+        // set the city
+        City.setCity("Little Rock")
+
+        // set state
+        State.setState("AR")
+        
+        // set number of players
+        Numberofplayers.setNumber_of_players(32)
+
+        System.out.printIn ("Name");
+        System.out.printIn ("Sport");
+        System.out.printIn ("City");
+        System.out.printIn ("State");
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
