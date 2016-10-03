d255c738532bc74804aa819081f58f8c4c94739c
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-10-03 14:53:57 -0500

    Flow & Operators Assignment - Final Draft

--- ACA2016/src/edu/uca/aca2016/Calhoun512/FlowOp.java
+++ ACA2016/src/edu/uca/aca2016/Calhoun512/FlowOp.java
@@ -16,30 +16,47 @@ public class FlowOp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-
-        for (int i = 0; i <= 200; i++){ {
+        
+        
+        /**
+         * The following line initiates the count, sets the parameters and 
+            increments, and prints the values. 
+            **/
+        
+        for (int i = 1; i <= 200; i++){ {
             System.out.println("count is:" + i);
             
         }
+        
+        // This line divides 10 from its multiples then prints the results.
 
             if (i % 10 == 0) {
                 System.out.println(i +" is divisible by 10. ");
 
             }
 
+        // This line further divides 50 by 5, then prints the reust
+        
             if (i == 50) {
                 int divide = i / 5;
                 System.out.println("50 divided by 5 is " + divide + ".");
 
             }
+        
+        // This line breaks the loop at '100' in the count 
+      
 
             if (i == 100) {
                 System.out.println(i + " is the final increment value.");
                 break;
                 
-            }
+            
         }
     }
+
+    
+    
+}
 
 }
 
