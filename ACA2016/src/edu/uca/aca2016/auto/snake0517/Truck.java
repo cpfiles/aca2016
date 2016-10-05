bd56939cdd2297aa195381868e56258f42a7b3a0
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-04 20:06:35 -0500

    Setup

--- ACA2016/src/edu/uca/aca2016/auto/snake0517/Truck.java
+++ ACA2016/src/edu/uca/aca2016/auto/snake0517/Truck.java
@@ -9,6 +9,47 @@ package edu.uca.aca2016.auto.snake0517;
  *
  * @author brela
  */
-public class Truck {
+public class Truck extends Automobile {
+     private double payload;
+     private double towing_capacity;
+     private boolean four_wheel_drive = true;
+
+    public Truck(double payload, double towing_capacity, int number_of_wheels, double weight_in_inches, double weight_in_pounds) {
+        super(number_of_wheels, weight_in_inches, weight_in_pounds);
+        this.payload = payload;
+        this.towing_capacity = towing_capacity;
+    }
+
+    public Truck() {
+        
+    }
+
+    public double getPayload() {
+        return payload;
+    }
+
+    public void setPayload(double payload) {
+        this.payload = payload;
+    }
+
+    public double getTowing_capacity() {
+        return towing_capacity;
+    }
+
+    public void setTowing_capacity(double towing_capacity) {
+        this.towing_capacity = towing_capacity;
+    }
+
+    public boolean isFour_wheel_drive() {
+        return four_wheel_drive;
+    }
+
+    public void setFour_wheel_drive(boolean four_wheel_drive) {
+        this.four_wheel_drive = four_wheel_drive;
+    }
+     
+    
+     
+     
     
 }
