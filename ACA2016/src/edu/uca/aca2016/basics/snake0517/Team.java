b4e2c7dbf9c19c46a154aec21799e3dc23658161
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-04 19:08:22 -0500

    Setup

--- ACA2016/src/edu/uca/aca2016/basics/snake0517/Team.java
+++ ACA2016/src/edu/uca/aca2016/basics/snake0517/Team.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.uca.aca2016.basics;
+package edu.uca.aca2016.basics.snake0517;
 
 /**
  *
