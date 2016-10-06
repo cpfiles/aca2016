c8850e32349f75d6f2f04e24b1515b634f933ec9
lokeee <lokeee@LAPTOP-JME1F18I.attlocal.net>
2016-10-05 19:42:12 -0500

    initial app break at 100 finishing up notes double checking

---
+++ ACA2016/src/edu/uca/aca2016/auto/lokeee/automobile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.auto.lokeee;
+
+/**
+ *
+ * @author lokeee
+ */
+public class automobile {
+    // setting number of wheels, height in inches and weight in pounds
+    private int number_of_wheels;
+    private float height_inches;
+    private float weight_lbs;
+    
+    
+}
---
+++ ACA2016/src/edu/uca/aca2016/auto/lokeee/truck.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.auto.lokeee;
+
+/**
+ *
+ * @author lokeee
+ */
+public class truck extends automobile{
+    // truck payload, towing capacity and if it is four wheel drive.
+    private int payload_lbs;
+    private int towing_capacity;
+    private boolean four_wheel_drive;
+    
+    
+}
