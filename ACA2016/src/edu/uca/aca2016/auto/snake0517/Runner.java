12314ed8e0ca1afd13b6a54833dccf5d3cc0c61a
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-05 20:08:42 -0500

    Setup

--- ACA2016/src/edu/uca/aca2016/auto/snake0517/Runner.java
+++ ACA2016/src/edu/uca/aca2016/auto/snake0517/Runner.java
@@ -15,7 +15,25 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Automobile myAutomobile = new Automobile();
+        
+        myAutomobile.setNumber_of_wheels(4);
+        myAutomobile.setHeight_in_inches(77.64);
+        myAutomobile.setWeight_in_pounds(4510);
+        
+        System.out.println("Number of wheels:" + " " + myAutomobile.getNumber_of_wheels());
+        System.out.println("Height in inches:" + " " + myAutomobile.getHeight_in_inches());
+        System.out.println("Weight in pounds:" + " " + myAutomobile.getWeight_in_pounds());
+        
+        Truck myTruck = new Truck();
+        
+        myTruck.setPayload(2020);
+        myTruck.setTowing_capacity(9400);
+        myTruck.setFour_wheel_drive(true);
+        
+        System.out.println("Payload:" + " " + myTruck.getPayload());
+        System.out.println("Towing capasity" + " " + myTruck.getTowing_capacity());
+        System.out.println("Four wheel drive" + " " + myTruck.isFour_wheel_drive());
     }
     
 }
