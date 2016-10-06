4fda59d6d0d6520d77cad8f8be6f42c623a64ed1
awood <awood@LAPTOP-RACFKBC7.philander.edu>
2016-10-05 20:17:46 -0500

    Run was successful

--- ACA2016/src/edu/uca/aca2016/auto/ACA1WOODRUFF/Runner.java
+++ ACA2016/src/edu/uca/aca2016/auto/ACA1WOODRUFF/Runner.java
@@ -5,6 +5,8 @@
  */
 package edu.uca.aca2016.auto.ACA1WOODRUFF;
 
+import edu.uca.aca2016.basics.Person;
+
 /**
  *
  * @author awood
@@ -15,7 +17,23 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Automobile corvette = new Automobile();
         // TODO code application logic here
+        corvette.getNumber_of_wheels(4);
+        corvette.getHeight(48);
+        corvette.getWeight(2800f);
+
+     System.out.println("The profile of 2002 Corvette Coup has" + getNumber_of_wheels(4);
+     System.out.println("at a height of" + getHeight (48);
+     System.out.println ("coming in at a weight of" + getWeight + "lbs.");
+     
+     
+    public Automobile(int number_of_wheels, double height, double weight) {
+        this.number_of_wheels = number_of_wheels;
+        this.height = height;
+        this.weight = weight;
+    }    
+        
     }
     
 }
