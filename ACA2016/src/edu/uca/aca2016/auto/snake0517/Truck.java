6941a6c1111c1b6c113a67227b09add575d9bff0
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-05 20:11:15 -0500

    Setup

--- ACA2016/src/edu/uca/aca2016/auto/snake0517/Truck.java
+++ ACA2016/src/edu/uca/aca2016/auto/snake0517/Truck.java
@@ -14,36 +14,71 @@ public class Truck extends Automobile {
      private double towing_capacity;
      private boolean four_wheel_drive = true;
 
+    /**
+     *
+     * @param payload
+     * @param towing_capacity
+     * @param number_of_wheels
+     * @param height_in_inches
+     * @param weight_in_pounds
+     */
     public Truck(double payload, double towing_capacity, int number_of_wheels, double height_in_inches, double weight_in_pounds) {
         super(number_of_wheels, height_in_inches, weight_in_pounds);
         this.payload = payload;
         this.towing_capacity = towing_capacity;
     }
 
+    /**
+     *
+     */
     public Truck() {
         
     }
 
+    /**
+     *
+     * @return
+     */
     public double getPayload() {
         return payload;
     }
 
+    /**
+     *
+     * @param payload
+     */
     public void setPayload(double payload) {
         this.payload = payload;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getTowing_capacity() {
         return towing_capacity;
     }
 
+    /**
+     *
+     * @param towing_capacity
+     */
     public void setTowing_capacity(double towing_capacity) {
         this.towing_capacity = towing_capacity;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isFour_wheel_drive() {
         return four_wheel_drive;
     }
 
+    /**
+     *
+     * @param four_wheel_drive
+     */
     public void setFour_wheel_drive(boolean four_wheel_drive) {
         this.four_wheel_drive = four_wheel_drive;
     }
