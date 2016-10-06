001124e6dead336b8b2218590bde9b72f81f9500
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-10-05 19:28:05 -0500

    Automobile Class

---
+++ ACA2016/src/edu/auto/calhoun512/AutoRunner.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.auto.calhoun512;
+
+/**
+ *
+ * @author calho
+ */
+public class AutoRunner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ACA2016/src/edu/auto/calhoun512/Truck.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.auto.calhoun512;
+
+/**
+ *
+ * @author calhoun512
+ */
+public class Truck extends AutomobileACS {
+    
+    
+    
+}
