35a034c4a930a677cb683be027260099a8dd6b45
awood <awood@LAPTOP-RACFKBC7.philander.edu>
2016-10-06 20:08:06 -0500

    Run was successful

--- ACA2016/src/edu/uca/aca2016/auto/ACA1WOODRUFF/Truck.java
+++ ACA2016/src/edu/uca/aca2016/auto/ACA1WOODRUFF/Truck.java
@@ -14,37 +14,78 @@ public class Truck extends Automobile{
     private double towing_capacity;
     private boolean four_wheel_drive = true;
 
+    /**
+     *
+     * @param payload
+     * @param towing_capacity
+     * @param number_of_wheels
+     * @param height
+     * @param weight
+     */
     public Truck(double payload, double towing_capacity, int number_of_wheels, double height, double weight) {
         super(number_of_wheels, height, weight);
         this.payload = payload;
         this.towing_capacity = towing_capacity;
     }
 
+    /**
+     *
+     * @param payload
+     * @param towing_capacity
+     */
     public Truck(double payload, double towing_capacity) {
         this.payload = payload;
         this.towing_capacity = towing_capacity;
     }
 
+    Truck() {
+        
+    }
+
+    /**
+     *
+     * @return
+     */
     public double getPayload() {
         return payload;
     }
 
+    /**
+     *
+     * @param payload
+     */
     public void setPayload(double payload) {
         this.payload = payload;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getTowing_capacity() {
         return towing_capacity;
     }
 
+    /**
+     *
+     * @param towing_capacity
+     */
     public void setTowing_capacity(double towing_capacity) {
         this.towing_capacity = towing_capacity;
     }
 
+    /**
+     *
+     * @return
+     */
     public boolean isFour_wheel_drive() {
         return four_wheel_drive;
     }
 
+    /**
+     *
+     * @param four_wheel_drive
+     */
     public void setFour_wheel_drive(boolean four_wheel_drive) {
         this.four_wheel_drive = four_wheel_drive;
     }
