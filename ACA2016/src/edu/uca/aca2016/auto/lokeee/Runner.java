f9f649e508a8c10e932bf81f64478651480be40c
lokeee <lokeee@LAPTOP-JME1F18I.attlocal.net>
2016-10-09 20:11:02 -0500

    Finished truck, runner, automobile classes Added

--- ACA2016/src/edu/uca/aca2016/auto/lokeee/Runner.java
+++ ACA2016/src/edu/uca/aca2016/auto/lokeee/Runner.java
@@ -15,7 +15,24 @@ public class Runner {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        // setting Ford as the automobile and declaring the values 
+        
+        Automobile Ford = new Automobile();
+        Ford.setnumber_of_wheels(4);
+        Ford.setheight_inches(45);
+        Ford.setweight_lbs(3505);
+       // printout to screen  
+      System.out.println("The Ford automobile has " +Ford.getnumber_of_wheels()+" wheels.");
+       System.out.println("The Ford automobile is " +Ford.getheight_inches() +" inches high.");
+        System.out.println("The Ford automobile weights " +Ford.getweight_lbs() +" pounds.");
+        // Setting truck to monster, declaring the values
+       Truck monster = new Truck();
+        monster.setpayload(2000);
+        monster.settowing(10000);
+	monster.setfourwd();
+       // printout to the screen
+		System.out.println("The monster truck has a payload of: " +getpayload());
+		System.out.println("The monster truck has a towing capacity of: " +gettowing());
+		System.out.println("is the monster truck four wheel drive? " +monster.setfourwd());
+}
 }
