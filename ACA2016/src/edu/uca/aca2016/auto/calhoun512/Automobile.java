6e89d8720d03c6098e6d97478e89db2fef61efe1
Chad Files <cfiles@pathagility.com>
2016-10-11 16:53:41 -0500

    moving code to where it should be

---
+++ ACA2016/src/edu/uca/aca2016/auto/calhoun512/Automobile.java
@@ -0,0 +1,16 @@
+package edu.uca.aca2016.auto.calhoun512;
+
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author calhoun512
+ */
+public class Automobile {
+    
+    
+}
