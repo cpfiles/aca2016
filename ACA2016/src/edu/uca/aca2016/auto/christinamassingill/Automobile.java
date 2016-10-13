887ec5c99e3a3ef31138d1406a24725792024b59
Chad Files <cfiles@pathagility.com>
2016-10-13 07:31:40 -0500

    adjusted pathing

--- ACA2016/src/edu/uca/aca2016/auto/christinamassingill/Automobile.java
+++ ACA2016/src/edu/uca/aca2016/auto/christinamassingill/Automobile.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package automobile;
+package edu.uca.aca2016.auto.christinamassingill;
 
 /**
  *
