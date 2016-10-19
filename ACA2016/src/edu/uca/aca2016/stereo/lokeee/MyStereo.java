57a162745dfc9a24bb8f554d74f190d2b02eed6c
lokeee <lokeee@gmail.com>
2016-10-19 15:48:33 -0500

    Initial App

---
+++ ACA2016/src/edu/uca/aca2016/stereo/lokeee/MyStereo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.stereo.lokeee;
+
+import edu.uca.aca2016.interfaces.Stereo;
+import java.util.Random;
+/**
+ *
+ * @author lokeee
+ */
+public class MyStereo {
+private boolean isPlaying =false;
+private boolean isPaused = false;
+private boolean isUSBLoaded = false;
+private boolean isStopped = false;
+private int Tracks;    
+
+    
+ public void loadUSB() {
+
+Random rand = new Random();
+
+int  Tracks =   rand.nextInt(1000)+1;
+System.out.println (Tracks);
+
+
+}
+
+}
