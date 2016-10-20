bb422d6010ed4776ce0363347ebece03f1152bfd
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-19 19:44:21 -0500

    Setup

--- ACA2016/src/edu/uca/aca2016/stereo/snake0517/TheStereo.java
+++ ACA2016/src/edu/uca/aca2016/stereo/snake0517/TheStereo.java
@@ -23,49 +23,52 @@ public class TheStereo implements Stereo {
     private boolean isPaused = false;
     private int current_track;
     private boolean isStopped = false;
-    
+
     @Override
     public void loadUSB() {
         int bound = 1000;
-        
+
         Random r = new Random();
-        
+
         number_of_tracks = r.nextInt(bound);
         number_of_tracks++;
         isUSBLoaded = true;
         current_track = 1;
+        isPlaying = true;
     }
 
     @Override
     public boolean isUSBLoaded() {
         return isUSBLoaded;
-        
+
     }
 
     @Override
     public void unloadUSB() {
-       isUSBLoaded = false;
-       this.number_of_tracks = 0;
-        
+        isUSBLoaded = false;
+        this.number_of_tracks = 0;
+        isPaused = false;
+        isPlaying = false;
+
     }
 
     @Override
     public int currentTrackNumber() {
-        
+
         return this.current_track;
     }
 
     @Override
     public int totalTrackCount() {
-        
+
         return this.number_of_tracks;
     }
 
     @Override
     public void enableStraightPlayMode() {
-       enableStraightPlayMode = true;
-       enableShufflePlayMode = false;
-       
+        enableStraightPlayMode = true;
+        enableShufflePlayMode = false;
+
     }
 
     @Override
@@ -78,17 +81,26 @@ public class TheStereo implements Stereo {
     public void stop() {
         isStopped = true;
         isPlaying = false;
+        isPaused = false;
     }
 
     @Override
     public void pause() {
-       isPaused = true;
-       isPlaying = false;
+        isPaused = true;
+        isPlaying = false;
     }
 
     @Override
     public void nextTrack() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (isUSBLoaded)
+        if (enableStraightPlayMode) {
+            this.current_track++;
+            if (this.current_track <= totalTrackCount()) {
+            } else {
+                this.current_track = 1;
+            }
+
+        }
     }
 
     @Override
@@ -101,15 +113,14 @@ public class TheStereo implements Stereo {
         isPaused = false;
         isStopped = false;
         return isPlaying = true;
-                
+
     }
 
     @Override
     public boolean isPaused() {
-       isPlaying = false;
-       isStopped = false;
-       return isPaused = true;
+        isPlaying = false;
+        isStopped = false;
+        return isPaused = true;
     }
-    
-    
+
 }
