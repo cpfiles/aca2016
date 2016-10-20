cd529556675601b665b7fa3a45a4c88d068618f6
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-10-19 19:59:57 -0500

    Setup

---
+++ ACA2016/src/edu/uca/aca2016/stereo/snake0517/Srun.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.stereo.snake0517;
+
+/**
+ *
+ * @author brela
+ */
+public class Srun {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TheStereo Tracks = new TheStereo();
+        
+        Tracks.loadUSB();
+        
+        System.out.println("Tracks " + Tracks.totalTrackCount());
+        
+//        Tracks.unloadUSB();
+        
+        System.out.println("Loaded " + Tracks.isUSBLoaded());
+        
+        Tracks.nextTrack();
+        
+        System.out.println("Track Number " + Tracks.currentTrackNumber());
+    }
+    
+}
