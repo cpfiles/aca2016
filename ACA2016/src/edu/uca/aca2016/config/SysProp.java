487afbe518159253b8bd10fe5c41fe32b84ec89d
jeffb <jeffb@LAPTOP-DQFQ3O1Q.Home>
2016-10-20 22:17:06 -0500

    added MyStereo and StereoRunner

--- ACA2016/src/edu/uca/aca2016/config/SysProp.java
+++ ACA2016/src/edu/uca/aca2016/config/SysProp.java
@@ -19,10 +19,10 @@ public class SysProp{
     public static void main(String[] args){
         Map<String, String> env = System.getenv();
         for (String envName : env.keySet()) {
-            System.out.format("%s=%s%n", envName, env.get(envName));
+//            System.out.format("%s=%s%n", envName, env.get(envName));
         }
         
         System.out.println("The HOME property is: " + System.getProperty("home"));
-        System.out.println("The user.home property is: " + System.getProperty("user.home"));
+//        System.out.println("The user.home property is: " + System.getProperty("user.home"));
     }
 }
