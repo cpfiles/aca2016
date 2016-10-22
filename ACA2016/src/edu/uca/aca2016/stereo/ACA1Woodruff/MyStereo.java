411b7c35e865465473ce2c3af16bf8ec98604bd0
awood <awood@LAPTOP-RACFKBC7.philander.edu>
2016-10-21 21:27:12 -0500

    Run was successful

--- ACA2016/src/edu/uca/aca2016/stereo/ACA1Woodruff/MyStereo.java
+++ ACA2016/src/edu/uca/aca2016/stereo/ACA1Woodruff/MyStereo.java
@@ -12,7 +12,9 @@ import java.util.Random;
  *
  * @author awood
  */
+
 public class MyStereo implements Stereo {
+//public class MyStereo extends Object implements Stereo
 
     private int NumberOfTracks;
     private boolean isUSBLoaded = false;
@@ -22,7 +24,7 @@ public class MyStereo implements Stereo {
     private boolean isPaused = false;
     private boolean isStopped = false;
     private int currentTrackNumber;
-
+//
     @Override
 
     public void loadUSB() {
@@ -34,39 +36,43 @@ public class MyStereo implements Stereo {
         isUSBLoaded = true;
         currentTrackNumber = 1;
     }
+//**This method should load mp3s from a USB drive. It will generate a 
+//random number of tracks between 1 and 1000.*/ 
 
     @Override
     public boolean isUSBLoaded() {
         return isUSBLoaded;
-        /**
-         * This method scans the USB drive for * mp3 files.
-         */
+       
     }
-
+//**This method should tell the caller if the USB drive has been scanned for mp3 files */
+    
     @Override
     public void unloadUSB() {
         isUSBLoaded = false;
         this.NumberOfTracks = 0;
     }
+//**This method should unload the USB drive by resetting all of the counts and counters.*/
 
     @Override
     public int currentTrackNumber() {
         return currentTrackNumber;
     }
+//**This method should return the number of the current track that is "playing".*/
 
     @Override
     public int totalTrackCount() {
         return NumberOfTracks;
     }
-
+//**Return the total number of tracks loaded from the USB drive.*/
     @Override
     public void enableStraightPlayMode() {
         enableStraightPlayMode = true;
         enableShufflePlayMode = false;
 
-
-        /*while loops needed*/
     }
+//**Plays the tracks in sequential order. This method should set the current track 
+//to the first track and keeps a record of the current track. Note the only way the 
+//program moves to the next track is by calls to nextTrack and previousTrack.*/
 
     @Override
     public void enableShufflePlayMode() {
@@ -74,20 +80,26 @@ public class MyStereo implements Stereo {
         enableStraightPlayMode = false;
 
     }
-
+//**Plays tracks in a random order. This method should set the current track to a random
+//track and keep a record of the current track. Note the only way the program moves to the 
+//next track is by calls to nextTrack and previousTrack.*/
+    
     @Override
     public void stop() {
         this.isPlaying = false;
         this.isStopped = true;
     }
-
+//**Stops the playing*/
+    
     @Override
     public void pause() {
         this.isPlaying = true;
         this.isStopped = false;
     }
-
+//*Pauses the playing*/
+    
     @Override
+    
     public void nextTrack() {
         if (isUSBLoaded && isPlaying) {
             if (enableStraightPlayMode) {
@@ -102,7 +114,8 @@ public class MyStereo implements Stereo {
             currentTrackNumber = r.nextInt((bound) + 1);
         }
     }
-
+//**Advances to next track*/
+    
     @Override
     public void previousTrack() {
         if (isUSBLoaded && isPlaying) {
@@ -118,7 +131,8 @@ public class MyStereo implements Stereo {
             currentTrackNumber = r.nextInt((bound) - 1);
         }
     }
-
+//**Moves to the next track/
+    
     @Override
     public boolean isPlaying() {
         isStopped = false;
@@ -132,4 +146,6 @@ public class MyStereo implements Stereo {
         isPlaying = false;
         return isPaused = true;
     }
+//**Tells the caller if the stereo is playing.*/
+
 }
