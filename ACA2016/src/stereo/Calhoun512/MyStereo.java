5653dfece9f8bbdeca357beaab8ba80d3fe76cd6
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-10-21 20:14:58 -0500

    MyStereo final Draft

--- ACA2016/src/stereo/Calhoun512/MyStereo.java
+++ ACA2016/src/stereo/Calhoun512/MyStereo.java
@@ -8,6 +8,8 @@ package stereo.Calhoun512;
 
 import edu.uca.aca2016.interfaces.Stereo;
 
+// Importing the Random Number Generator utility (I'm thankful they included)
+
 import java.util.Random;
 
 /**
@@ -30,7 +32,10 @@ public class MyStereo implements Stereo {
     
 
     @Override
-    
+    /**
+     * The following lines load the data/songs to the player and determine the
+     * total number of tracks.  In this case, a number generated at "random."
+     */
     public void loadUSB() {
         
         int bound = 1000;
@@ -46,7 +51,7 @@ public class MyStereo implements Stereo {
      @Override  
         
             
-   
+   // This line returns a true value if and when the data has been loaded
 
     public boolean isUSBLoaded() {
         
@@ -55,6 +60,11 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    /* These lines deal with the data being unloaded.  Upon the storage device
+        being removed it resets the Track Count to zero.
+    */
+        
     public void unloadUSB() {       
         isUSBLoaded = false;
         this.NumberOfTracks = 0;
@@ -63,6 +73,11 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    /* These lines tell you what Track is currently playing, assuming the data
+        has been loaded into the player.
+    */
+    
     public int currentTrackNumber() {
         if (isUSBLoaded){}
         return this.currentTrackNumber;
@@ -70,6 +85,11 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    /* These lines give the user the total amount of tracks on the storage device.
+        Thus enabling the user and the program to navigate the data.
+    */
+    
     public int totalTrackCount() {
         if (isUSBLoaded){}
         return this.NumberOfTracks;
@@ -77,6 +97,9 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    // These lines turn on Straight Play Mode to play the tracks consecutively.
+    
     public void enableStraightPlayMode() {
         if (isUSBLoaded){}
         this.enableStraightPlayMode = true;
@@ -84,15 +107,22 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    //These lines enable shuffle, randomly queing the next track.
+    
     public void enableShufflePlayMode() {
         if (isUSBLoaded){}
         this.enableShufflePlayMode = true;
         this.enableStraightPlayMode = false;
         
         
+        
     }
 
     @Override
+    
+    // This stops the stereo from playing.
+    
     public void stop() {
        if (isUSBLoaded){
        isStopped = true;
@@ -102,6 +132,9 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    // This pauses the stereo.
+    
     public void pause() {
         if (isUSBLoaded){
         isPaused = true;
@@ -110,6 +143,10 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    /* These lines advance to the next track and also differentiate how to do so
+        whether the player is set to Shuffle or Straight-Play.
+    */
     public void nextTrack() {
       if (isUSBLoaded){
         if (enableStraightPlayMode) {
@@ -130,6 +167,11 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    /* These lines replay the last-played track in Straight-Play mode, or randomly
+        select a new track to be played in Shuffle mode.        
+    */
+    
     public void previousTrack() {
             if (isUSBLoaded){
         if (enableStraightPlayMode) {
@@ -151,6 +193,9 @@ public class MyStereo implements Stereo {
 
         
     @Override
+    
+    // These lines indicate whether the stereo is playing and play it if it is not
+    
     public boolean isPlaying() {
         if (isUSBLoaded){
         isPaused = false;
@@ -163,6 +208,9 @@ public class MyStereo implements Stereo {
     }
 
     @Override
+    
+    //  These lines pause the Stereo. 
+    
     public boolean isPaused() {
         if (isUSBLoaded){
         isPlaying = false;
