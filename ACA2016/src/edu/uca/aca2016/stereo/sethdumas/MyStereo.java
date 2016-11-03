054541fcd5f37537ec9b2cb1252eab96d9c3c7cb
sethdumas <sethdumas@gmail.com>
2016-11-03 15:43:21 -0500

    MyStereo

--- ACA2016/src/edu/uca/aca2016/stereo/sethdumas/MyStereo.java
+++ ACA2016/src/edu/uca/aca2016/stereo/sethdumas/MyStereo.java
@@ -13,150 +13,132 @@ public class MyStereo implements Stereo {
     private boolean isUSBLoaded = false;
     private boolean isPlaying = false;
     private boolean isPlayingStraight = false;
-    private boolean isPlayingShuffle = false;
     private boolean isPaused = false;
     private boolean isStopped = false;
 
     private static final Logger logger = Logger.getLogger(edu.uca.aca2016.stereo.sethdumas.MyStereo.class.getName());
-   
-    
+
 //load a random number of mp3s
     @Override
     public void loadUSB() {
-        isUSBLoaded = true;
-        isPlaying = true;
-        isPlayingStraight = true;
+        this.isUSBLoaded = true;
+
         Random rand = new Random();
-        TotalTracks = (rand.nextInt(1000) + 1);
-        while (TotalTracks == 0) {
-            TotalTracks++;
-            break;
-        }
+        this.TotalTracks = rand.nextInt(1000);
+        this.TotalTracks++;
+        this.CurrentTrack = 1;
+        this.isPlayingStraight = true;
+        this.isPlaying = true;
     }
 
     //tell if USB drive has been loaded
     @Override
-    public boolean isUSBLoaded(){
-        isUSBLoaded = true;
-        while (isUSBLoaded = true) {
-            isPlaying = true;
-        }
-        return isUSBLoaded;
+    public boolean isUSBLoaded() {
+        return this.isUSBLoaded;
     }
 
     //unload USB drive and reset
     @Override
     public void unloadUSB() {
-        isUSBLoaded = false;
-        while (isUSBLoaded == false) {
-            TotalTracks = 0;
-            CurrentTrack = 0;
-            isPlayingStraight = false;
-            isPlayingShuffle = false;
-            isPlaying = false;
-            isPaused = false;
-            isStopped = false;
-            break;
-        }
+        this.isUSBLoaded = false;
+        this.TotalTracks = 0;
+        this.CurrentTrack = 0;
+        this.isPlayingStraight = false;
+        this.isPlaying = false;
+        this.isPaused = false;
+        this.isStopped = false;
     }
 
     //return current track number
     @Override
     public int currentTrackNumber() {
-        return CurrentTrack;
+        return this.CurrentTrack;
     }
 
     //return the totaal number of tracks
     @Override
     public int totalTrackCount() {
-        return TotalTracks;
+        return this.TotalTracks;
     }
 
     //play tracks in sequential order
     @Override
     public void enableStraightPlayMode() {
-        isPlaying = true;
-        isPlayingStraight = true;
-        isPlayingShuffle = false;
-        isPaused = false;
-        isStopped = false;
+
+        this.isPlayingStraight = true;
     }
 
     //play the tracks in a random order
     @Override
     public void enableShufflePlayMode() {
-        isPlaying = true;
-        isPlayingStraight = false;
-        isPlayingShuffle = true;
-        isPaused = false;
-        isStopped = false;
+        this.isPlayingStraight = false;
     }
 
     //stop playing
     @Override
     public void stop() {
-        isStopped = true;
+        this.isStopped = true;
+        this.isPlaying = false;
+        this.isPaused = false;
     }
 
     //pause playing
     @Override
     public void pause() {
-        isPaused = true;
-        isPlaying = false;
-        isStopped = false;
+        this.isPaused = true;
+        this.isPlaying = false;
+        this.isStopped = false;
     }
 
     //advance to next track
     @Override
     public void nextTrack() {
-        isPaused = false;
-        isStopped = false;
-        if (isUSBLoaded == true) {
-            if (isPlayingStraight == true) {
-                CurrentTrack++;
-                if (CurrentTrack > TotalTracks);
-                {
-                    this.CurrentTrack = 1;
-                }
-                if (isPlayingShuffle == true) {
-                    Random nextShuffle = new Random();
-                    CurrentTrack = (nextShuffle.nextInt(TotalTracks++));
-                }
-            }
-
+        if (!this.isUSBLoaded && !this.isPlaying) {
+            return;
         }
+        if (this.isPlayingStraight) {
+            this.CurrentTrack++;
 
+            if (this.CurrentTrack > this.TotalTracks) {
+                this.CurrentTrack = 1;
+            }
+        } else {
+            Random rand = new Random();
+            this.CurrentTrack = rand.nextInt(this.TotalTracks);
+            this.CurrentTrack++;
+        }
     }
 
-    //revert to previous track
-    @Override
-    public void previousTrack() {
-        if (isUSBLoaded = true) {
-            if (isPlayingStraight = true) {
-                CurrentTrack--;
-                if (CurrentTrack < 1);
-                {
-                    CurrentTrack = TotalTracks;
-                }
-            }
+//revert to previous track
+@Override
+        public void previousTrack() {
+        if (!this.isUSBLoaded && !this.isPlaying) {
+            return;
         }
+            
+        if (this.isPlayingStraight) {
+                this.CurrentTrack--;
+                
+                if (this.CurrentTrack == 0) {
+                    this.CurrentTrack = this.TotalTracks;
+                }
+        }    
+        else {
+            Random rand = new Random();
+            this.CurrentTrack = rand.nextInt(this.TotalTracks);
+            this.CurrentTrack++;
+        }      
     }
 
     //return whether or not track is playing
     @Override
-    public boolean isPlaying() {
-        isPlaying = true;
-        isStopped = false;
-        isPaused = false;
-        return isPlaying;
+        public boolean isPlaying() {
+        return this.isPlaying;
     }
 
     //return whether or not track is paused
     @Override
-    public boolean isPaused() {
-        isPaused = true;
-        isPlaying = false;
-        isStopped = false;
-        return isPaused;
+        public boolean isPaused() {
+        return this.isPaused;
     }
 }
