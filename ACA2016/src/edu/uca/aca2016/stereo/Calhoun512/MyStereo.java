9250c41dd9befe0aaf58877d4b93e6b1d4eb3ad6
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-11-04 22:18:24 -0500

    MyStereo final Draft

--- ACA2016/src/edu/uca/aca2016/stereo/Calhoun512/MyStereo.java
+++ ACA2016/src/edu/uca/aca2016/stereo/Calhoun512/MyStereo.java
@@ -21,6 +21,7 @@ import java.util.List;
 // Importing the Random Number Generator utility (I'm thankful they included)
 
 import java.util.Random;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
@@ -73,6 +74,9 @@ public class MyStereo implements StereoExtended {
         
             
    // This line returns a true value if and when the data has been loaded
+     /**
+      * Loads USB and returns True if the data is already loaded.
+      */
 
     public boolean isUSBLoaded() {
         
@@ -83,8 +87,8 @@ public class MyStereo implements StereoExtended {
 
     @Override
     
-    /* These lines deal with the data being unloaded.  Upon the storage device
-        being removed it resets the Track Count to zero.
+    /** Unloads the Data.  Upon the storage device
+    *    being removed it resets the Track Count to zero.
     */
         
     public void unloadUSB() {       
@@ -98,11 +102,11 @@ public class MyStereo implements StereoExtended {
     }
 
     @Override
-    
-    /* These lines tell you what Track is currently playing, assuming the data
-        has been loaded into the player.
-    */
-    
+    /**
+     * returns the current track number
+     * 
+     * @return
+     */
     public int currentTrackNumber() {
         return this.currentTrackNumber;
        
@@ -110,9 +114,11 @@ public class MyStereo implements StereoExtended {
 
     @Override
     
-    /* These lines give the user the total amount of tracks on the storage device.
-        Thus enabling the user and the program to navigate the data.
-    */
+    /**
+     * returns the total number of tracks
+     * 
+     * @return
+     */
     
     public int totalTrackCount() {
         return this.NumberOfTracks;
@@ -123,6 +129,10 @@ public class MyStereo implements StereoExtended {
     
     // These lines turn on Straight Play Mode to play the tracks consecutively.
     
+    /**
+     * enables straight play mode, disables Shuffle mode
+     */
+    
     public void enableStraightPlayMode() {
         this.enableStraightPlayMode = true;
         this.enableShufflePlayMode = false;
@@ -131,6 +141,10 @@ public class MyStereo implements StereoExtended {
     @Override
     
     //These lines enable shuffle, randomly queing the next track.
+    
+    /**
+     * Enables shuffle mode, disables straight play mode
+     */
     
     public void enableShufflePlayMode() {
         this.enableShufflePlayMode = true;
@@ -144,6 +158,10 @@ public class MyStereo implements StereoExtended {
     
     // This stops the stereo from playing.
     
+    /**
+     * Stops the Stereo, makes false isPlaying and isPaused
+     */
+    
     public void stop() {
        isStopped = true;
        isPlaying = false;
@@ -155,6 +173,10 @@ public class MyStereo implements StereoExtended {
     @Override
     
     // This pauses the stereo.
+    
+    /**
+     * pauses the stereo, makes false isPlaying and isStopped
+     */
     
     public void pause() {
         isPaused = true;
@@ -168,6 +190,11 @@ public class MyStereo implements StereoExtended {
     /* These lines advance to the next track and also differentiate how to do so
         whether the player is set to Shuffle or Straight-Play.
     */
+    
+    /**
+     * Advances to the next track and differentiates functions between Shuffle and 
+     * Straight play mode.
+     */
     public void nextTrack() {
       if (isUSBLoaded){
         if (enableStraightPlayMode) {
@@ -192,6 +219,11 @@ public class MyStereo implements StereoExtended {
     /* These lines replay the last-played track in Straight-Play mode, or randomly
         select a new track to be played in Shuffle mode.        
     */
+    
+    /**
+     * regresses to the previous track, differentiates between straight play and 
+     * shuffle mode.
+     */
     
     public void previousTrack() {
             if (isUSBLoaded){
@@ -218,11 +250,16 @@ public class MyStereo implements StereoExtended {
     
     // These lines indicate whether the stereo is playing and play it if it is not
     
+    /**
+     * plays the stereo, makes false isPaused and isStopped.
+     * 
+     * @return
+     */
     public boolean isPlaying() {
-        if (isUSBLoaded){
+      
         isPaused = false;
         isStopped = false;
-        }
+        
         
         return isPlaying = true;
         
@@ -233,16 +270,26 @@ public class MyStereo implements StereoExtended {
     
     //  These lines pause the Stereo. 
     
+    /**
+     * Pauses the stereo.
+     * 
+     * @return
+     */
+    
     public boolean isPaused() {
-        if (isUSBLoaded){
+       
         isPlaying = false;
         isStopped = false;
-        }
+        
         return isPaused = true;
         
     }
 
     @Override
+    
+    /**
+     * Loads the audio files and starts the stereo.
+     */
     public void loadTrackList(File trackListSource)throws IOException {
        
         Scanner s = null; 
@@ -256,7 +303,11 @@ public class MyStereo implements StereoExtended {
             
             
         }catch (IOException ex) {
-            new Exception ("IO Error:" + ex.getMessage());           
+            try {           
+                throw new Exception ("IO Error:" + ex.getMessage());
+            } catch (Exception ex1) {
+                Logger.getLogger(MyStereo.class.getName()).log(Level.SEVERE, null, ex1);
+            }
         } finally {
             if (s != null) {
                 s.close();
@@ -266,6 +317,10 @@ public class MyStereo implements StereoExtended {
     }
 
     @Override
+    
+    /**
+     * Starts the stereo to play.  Sets stopped and Paused to false.
+     */
     public void play() {
         isPaused = false;
         isStopped = false;
@@ -273,13 +328,23 @@ public class MyStereo implements StereoExtended {
     }
 
     @Override
+    
+    /**
+     * Gives the names of the tracks that have been.
+     * 
+     * @return
+     */
     public ArrayList<String> getTrackList() {
         return this.trackList;
     }
             
     
     
-
+/**
+ * Displays the current track's name
+ * 
+ * @return 
+ */
 
     @Override
     public String getCurrentTrackFileName() {
