d14601f083ab7dea352400961d7aecd118fb4e54
Username <Username@10.108.1.90>
2016-11-04 20:22:55 -0500

    004

--- ACA2016/src/edu/uca/aca2016/stereo/clintlemons/MyStereo.java
+++ ACA2016/src/edu/uca/aca2016/stereo/clintlemons/MyStereo.java
@@ -6,15 +6,24 @@
 package edu.uca.aca2016.stereo.clintlemons;
 import edu.uca.aca2016.interfaces.StereoExtended;
 import java.io.File;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
 import java.io.IOException;
 import java.util.Random;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Collection;
-import java.util.Collections;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+//import java.util.List;
+//import java.util.ListIterator;
+import java.util.Scanner;
+//import java.util.Collection;
+//import java.util.Collections;
+//import java.io.ObjectInput;
+//import java.io.ObjectOutput;
+//import java.io.InputStream;
+//import java.io.OutputStream;
+//import java.io.FileNotFoundException;
+//import static java.util.Collections.enumeration;
+//import static java.util.Spliterators.iterator;
+
         
 
 /**
@@ -23,7 +32,9 @@ import java.io.ObjectOutput;
  */
 public class MyStereo implements StereoExtended {
       
-    //Constructor variables
+    /**
+     * Constructor variables
+     */
     private boolean Is_USB_Loaded;
     private boolean Load_USB;
     private int Max_Track;
@@ -35,8 +46,13 @@ public class MyStereo implements StereoExtended {
     private boolean Is_Stopped;
     private boolean Enable_Shuffle_Play;
     private boolean Enable_Straight_Play;
-    //Initializer variables
+    ArrayList<String> Track_List = new ArrayList<>();
+    String Current_Track_Name = new String();
     
+
+    /**
+     *Initializer variables
+     */
     public MyStereo() {
         this.Enable_Straight_Play = false;
         this.Enable_Shuffle_Play = false;
@@ -108,23 +124,32 @@ public class MyStereo implements StereoExtended {
         Is_Paused = false;
         
     }
-    //return current track
+    /**
+    * 
+    * @return current track
+    */
     @Override
     public int currentTrackNumber() {
         return Current_Track;
     }
-    //return total number of tracks
+    /**
+     * @return total number of tracks
+     * */
     @Override
     public int totalTrackCount() {
         return Max_Track;
     }
-    //Enables straight play mode
+    /**
+     * Enables straight play mode
+     */
     @Override
     public void enableStraightPlayMode() {
         Enable_Straight_Play = true;
         Enable_Shuffle_Play = false;
     }
-    //Enables shuffle play mode
+    /*
+     *Enables shuffle play mode
+    */
     @Override
     public void enableShufflePlayMode() {
         Enable_Shuffle_Play=true;
@@ -152,26 +177,58 @@ public class MyStereo implements StereoExtended {
     public boolean isPaused() {
         return Is_Paused;
     }
-    //**
-
+    /**
+     * 
+     * @param trackListSource to load track list
+     * @throws IOException 
+    */
     @Override
     public void loadTrackList(File trackListSource) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        Scanner s = null;
+        this.Is_USB_Loaded=true;
+        this.Is_Playing=true;
+        this.Enable_Straight_Play=true;
+       
+        try {
+            s = new Scanner(trackListSource);
+             while (s.hasNextLine()){
+                 this.Track_List.add(s.nextLine());
+            }
+            this.Current_Track = (this.Track_List.size()-1);
+            s.close();            
+            }
+        catch (IOException ex){
+            throw new IOException ("IO error:" + ex.getMessage());
+        }
+        } 
 
+   /** define states of play mode
+    * 
+    */
     @Override
-    public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void play(){
+        this.Is_Playing = true;
+        this.Is_Paused = false;
+        this.Is_Stopped = false;
     }
 
     @Override
     public ArrayList<String> getTrackList() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.Track_List.isEmpty()){
+        return null;
     }
-
+        else{
+            return this.Track_List;
+        }
+    }
+    
     @Override
     public String getCurrentTrackFileName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.Track_List.isEmpty()){
+            return null;
+            }
+        else{
+            return this.Current_Track_Name = this.Track_List.get(Min_Track);
+        }
     }
 }
-                    
