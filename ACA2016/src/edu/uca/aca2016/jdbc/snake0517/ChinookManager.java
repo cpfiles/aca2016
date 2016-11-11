411b70df507f56380708c66c61a76619ea9c1c6e
brela <brela@DESKTOP-9E66JR8.attlocal.net>
2016-11-10 20:20:57 -0600

    004

---
+++ ACA2016/src/edu/uca/aca2016/jdbc/snake0517/ChinookManager.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.jdbc.snake0517;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.sql.*;
+
+/**
+ *
+ * @author brela
+ */
+public class ChinookManager {
+    
+    private Properties Chin = new Properties();
+    
+    public ChinookManager() throws IOException, SQLException {
+        FileInputStream in = null;
+        try {
+            Path inpath = Paths.get("resources", "config", "Snake0517", "ChinookManager.prperties");
+            in = new FileInputStream(inpath.toFile());
+            this.Chin.load(in);
+            in.close();
+        } finally {
+            
+            if (in != null) {
+                in.close();
+            }
+            Connection con = DriverManager.getConnection(Chin.getProperty("db.connection"));
+        }
+    }
+}
