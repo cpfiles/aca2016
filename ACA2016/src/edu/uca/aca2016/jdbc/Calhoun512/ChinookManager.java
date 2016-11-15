94187b227a374cd5e991e9fc1df7d05d726b1174
calho <calho@LAPTOP-243QP7S1.philander.edu>
2016-11-14 19:19:33 -0600

    Chinook Manager

--- ACA2016/src/edu/uca/aca2016/jdbc/Calhoun512/ChinookManager.java
+++ ACA2016/src/edu/uca/aca2016/jdbc/Calhoun512/ChinookManager.java
@@ -16,16 +16,22 @@ import java.io.File;
 import java.nio.Path;
 import java.nio.Paths;
 import java.io.FileInputStream;
-import ChinookManager.properties;
+import edu.uca.aca2016.jdbc.Calhoun512.ChinookManager.properties;
+import java.sql.Statement;
 
 
 
 public class ChinookManager{
 
-    private Properties chinookManager = new Properties();
+    private Properties chinook = new Properties();
 
-    public Properties(){
-        this.loadDefaultProperties();
+    public chinookManager() throws IOException, SQLException {
+        FileInputStream in = null;
+        try{
+            Path inpath = Paths.get("resources", "config", "Calhoun512", "ChinookManager");
+            in = new FileInputStream(inpath.toFile()};
+            this.chinook.load(in);
+        
         
         System.out.println(this.chinookManager.getProperty("db.connection"));
         }
