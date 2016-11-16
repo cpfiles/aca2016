9e8a5410ab14121179d4077c89e630b9810ee8e1
sethdumas <sethdumas@gmail.com>
2016-11-15 18:25:17 -0600

    005

--- ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
+++ ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
@@ -5,10 +5,36 @@
  */
 package edu.uca.aca2016.jdbc.sethdumas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author sethd
  */
 public class ChinookManager {
     
+//Create a class constructor that takes zero parameters    
+public void connectToAndQueryDatabase(String url) throws SQLException{
+        Connection con = DriverManager.getConnection(url);
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Customer WHERE CustomerID > 55");
+
+        while(rs.next()){
+            String first_name = rs.getString("FirstName");
+            String last_name = rs.getString("LastName");
+            String email = rs.getString("Email");
+            int id = rs.getInt("CustomerId");
+            
+            System.out.format("Customer: %d\t%-30.30s %-30.30s %-30.30s %n", id, first_name, last_name, email);
+        }
+        
+        stmt.close();
+        con.close();
+    }
+
 }
