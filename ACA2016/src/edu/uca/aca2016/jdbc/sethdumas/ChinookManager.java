b77a0e0c0cb729ba6a9aa557e2ce300d62f74857
sethdumas <sethdumas@gmail.com>
2016-11-16 19:27:52 -0600

    005

--- ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
+++ ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
@@ -6,7 +6,6 @@
 package edu.uca.aca2016.jdbc.sethdumas;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,31 +21,27 @@ import java.util.logging.Logger;
  * @author sethd
  */
 public class ChinookManager {
-    private Properties loadChinookMAnager = new Properties();
 
-    private void loadChinookManager() {
-        FileInputStream in = null;
+    Connection con;
+    private static final Logger logger = Logger.getLogger(ChinookManager.class.getName());
 
+    public void ChinookManager() {
         try {
-            Path inpath = Paths.get("resources","config","sethdumas","ChinookManager.properties");
-            in = new FileInputStream(inpath.toFile());
-            this.ChinookManager.load(in);
-            in.close();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, "Properties file was not found", ex);
-        } catch (IOException ex) {
-            Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, "Exception reading properties file", ex);
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, null, ex);
-
+            Path inpath = Paths.get("resources", "config", "sethdumas", "ChinookManager.properties");
+            Properties props;
+            try (FileInputStream in = new FileInputStream(inpath.toFile())) {
+                props = new Properties();
+                props.load(in);
             }
+
+            con = DriverManager.getConnection(props.getProperty("db.connection"));
+
+        } catch (SQLException | IOException ex) {
+            Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    public void addArtist(){
+        
+        
+    }
 }
-
-
---
+++ ACA2016/test/edu/uca/aca2016/jdbc/sethdumas/ChinookManagerTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.jdbc.sethdumas;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author sethd
+ */
+public class ChinookManagerTest {
+    
+    public ChinookManagerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of ChinookManager method, of class ChinookManager.
+     */
+    @Test
+    public void testChinookManager() {
+        System.out.println("ChinookManager");
+        ChinookManager instance = new ChinookManager();
+        instance.ChinookManager();
+        // TODO review the generated test code and remove the default call to fail.
+        assertTrue("");
+    }
+    
+}
