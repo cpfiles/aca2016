d0bab55b6569c70233050b6ed3d438f1ef5a627e
Username <Username@10.108.1.90>
2016-11-17 18:59:46 -0600

    save

--- ACA2016/src/edu/uca/aca2016/jdbc/clintlemons/ChinookManager.java
+++ ACA2016/src/edu/uca/aca2016/jdbc/clintlemons/ChinookManager.java
@@ -11,12 +11,12 @@ import java.nio.file.Paths;
 import java.io.FileInputStream;
 import java.io.IOException;
 import static java.lang.System.in;
-import java.sql.Connection;
+import java.sql.*;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Dictionary;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//import java.util.Dictionary;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.io.FileNotFoundException;
@@ -26,27 +26,44 @@ import java.io.FileNotFoundException;
  */
 public class ChinookManager {
     Connection con=null;
-    private Properties defaultProperties =new Properties();
-    
-        public void ChinookManager(){
-            this.loadDefaultProperties();
-            System.out.println(this.defaultProperties.getProperty("ChinookManager.properties"));
-        }
-
-    private void loadDefaultProperties() {
-        FileInputStream in = null;
-        
+    private final Properties defaultProperties =new Properties();
+    //this.con = DriverManager.getConnection("dbconnection");
+    /**
+     *loads properties file
+     * @param dbconnection
+     * @throws java.sql.SQLException
+     * 
+     */
+    public void ChinookManager (String dbconnection) throws SQLException {
+        FileInputStream in;
+        this.con = DriverManager.getConnection("dbconnection");
         try{
-            Path inpath = Paths.get("resources","config","ChinookManager.properties");
+            Path inpath = Paths.get("resources","config","clintlemons","ChinookManager.properties");
             in = new FileInputStream(inpath.toFile());
             this.defaultProperties.load(in);
+            //con = defaultProperties.getProperty("db.connection",));
+            //this.con.prepareCall("Chinook_db");
             in.close();
-        } catch (FileNotFoundException Properties) {
-            Logger.getLogger(ChinookManager.class.getName()).log(Level.WARNING, null, Properties);
-        } catch (IOException Properties) {
-            Logger.getLogger(ChinookManager.class.getName()).log(Level.WARNING, null, Properties);
+        } catch (FileNotFoundException Properties_Not_Found) {
+            Logger.getLogger(ChinookManager.class.getName()).log(Level.WARNING, null, Properties_Not_Found);
+        } catch (IOException Properties_IO_Error) {
+            Logger.getLogger(ChinookManager.class.getName()).log(Level.WARNING, null, Properties_IO_Error);
         }
+        
+    }
+    public void addArtist(String name){
+              logger.info("Added artist'"+name+"' to the database");
+    }
+
+    ChinookManager (){
+        throw new UnsupportedOperationException("connection failed"); //To change body of generated methods, choose Tools | Templates.
+    }
 }
+
+
+
+
+
 //        private Properties prop=new Properties(){
 //                String Connection = prop.getProperty("ChinookManager.properties");
 //                System.out.println("Connection Successful");
@@ -56,19 +73,10 @@ public class ChinookManager {
 //        } catch (IOException ex) {
 //            Logger.getLogger(ChinookManager.class.getName()).log(Level.WARNING, null, ex);
 //        }
-        try {
-            in.close();
-        } catch (IOException ex) {
-            Logger.getLogger(ChinookManager.class.getName()).log(Level.INFO, null, ex);
-        }
-               
-    }
-}
-
-           
-    
-    
-    
+//        try {
+//            in.close();
+//        } catch (IOException ex) {
+//            Logger.getLogger(ChinookManager.class.getName()).log(Level.INFO, null, ex);
 //     Connection con;
 //    public ChinookManager() throws SQLException {
 //        this.con = DriverManager.getConnection("ChinookManager.properties");   
@@ -133,8 +141,21 @@ public class ChinookManager {
 //    }
 //  
 //            
+ // @cpfiles ex---public void addArtist(String name){
+        //         logger.info("Added artist'"+name+"' to the database");
+    //}
+   // @cpfiles ex ---public int getArtist(String name)
+   //                logger.info("not implemented");
+   //                  return -1:
+    //}
+//    //@cpfiles ex was on or around line 31 
+//       private static final Logger logger = Logger.getLogger(ChinookManager.class.getName());
 
 
-
-    
-
+    //this.con = load.DriverManager
+   // public void main connectChinookManager() {
+        //this.con = DriverManager.getConnection((jdbc:sqlite:)) + load.ChinookManager.properties);
+//        public void ChinookManager(){
+//            this.loadDefaultProperties();
+//           System.out.println(this.defaultProperties.getProperty("ChinookManager.properties"));
+//        }
