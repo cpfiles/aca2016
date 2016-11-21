9ab4bd25229fde1f8e5b790edb98d5eb87b63898
awood <awood@LAPTOP-RACFKBC7.philander.edu>
2016-11-20 23:36:14 -0600

    Merge master into ACA1Woodruff004
    Conflicts:
    	ACA2016/src/edu/uca/aca2016/stereo/ACA1Woodruff/MyStereo.java
    	ACA2016/src/edu/uca/aca2016/stereo/cpfiles/MyStereo.java

---
+++ ACA2016/src/edu/uca/aca2016/jdbc/ACA1Woodruff/Chinook.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.jdbc.ACA1Woodruff;
+
+import java.io.FileInputStream;
+
+/**
+ *
+ * @author awood
+ */
+class Chinook {
+
+    static void load(FileInputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static String getProperty(String dbconnection) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
