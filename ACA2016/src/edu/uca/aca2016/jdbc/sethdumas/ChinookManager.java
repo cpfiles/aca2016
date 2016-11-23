8b65927ab3854d9fdbfe090e482111c73a29dd97
sethdumas <sethdumas@gmail.com>
2016-11-22 19:36:00 -0600

    005

--- ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
+++ ACA2016/src/edu/uca/aca2016/jdbc/sethdumas/ChinookManager.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -26,57 +27,94 @@ public class ChinookManager {
     Connection con;
     private static final Logger logger = Logger.getLogger(ChinookManager.class.getName());
 
-    public void ChinookManager() {
+    public ChinookManager() {
         try {
             Path inpath = Paths.get("resources", "config", "sethdumas", "ChinookManager.properties");
             Properties props;
-            try (FileInputStream in = new FileInputStream(inpath.toFile())) {
-                props = new Properties();
-                props.load(in);
-            }
+
+            FileInputStream in = new FileInputStream(inpath.toFile());
+            props = new Properties();
+            props.load(in);
+
 
             con = DriverManager.getConnection(props.getProperty("db.connection"));
+            System.out.println(props.getProperty("db.connection"));
+            
 
         } catch (SQLException | IOException ex) {
             Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
     //addArtist has a single parameter for the artist’s name. 
     //This method should insert a row into the Artist database table 
     //in order to make a new Artist record.
-
-    public void addArtist(String addArtist) throws SQLException{
+    public void addArtist(String ArtistName) throws SQLException {
         //Connection con = null;
         PreparedStatement ps = null;
-        
-        try{
+
+        try {
             //con = DriverManager.getConnection(props.getProperty("db.connection"));
-            String sql = "INSERT INTO Artist (Name)" + "VALUES (?)";
+            String sql = "INSERT INTO Artist (Name) VALUES (?)";
             ps = con.prepareStatement(sql);
-            ps.setString(1, addArtist);
+            ps.setString(1, ArtistName);
             ps.executeUpdate();
-                                    
+
         } catch (SQLException ex) {
             Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        finally {
+        } finally {
             if (ps != null) {
                 ps.close();
             }
-        
+
+        }
+
     }
-}
-    // getArtist takes a single parameter for the artist’s name. 
-    //The method needs to query the Artist table and attempt to find the 
-    //artist the user requested. This needs to be a case insensitive search. 
-    //The method should return the ID of the artist if one is found. 
-    //If there is no match or there are multiple rows returned the method should return -1.
+// getArtist takes a single parameter for the artist’s name. 
+//The method needs to query the Artist table and attempt to find the 
+//artist the user requested. This needs to be a case insensitive search. 
+//The method should return the ID of the artist if one is found. 
+//If there is no match or there are multiple rows returned the method should return -1.
 
 //    public void getArtist() throws SQLException{
 //        
 //    } 
-}
 //get artist -row count - you have to know how many rows to come back.
 //
+    public int getArtist(String ArtistName) throws SQLException {
+        PreparedStatement ps = null;
+        int ArtistId = -1;
+        String sql = "SELECT * FROM Artist WHERE UPPER (Name) = (?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, ArtistName.toUpperCase());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                ArtistId = rs.getInt("ArtistId");
+            }
+            if (rs.next()) {
+                ArtistId = -1;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ChinookManager.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            if (ps != null) {
+                ps.close();
+            }
+        }
+        return ArtistId;
+    }
+    //find the artist record with the id 
+    //provided, then update the name of the 
+    //artist with the name provided. The method 
+    //should return a boolean; true if the 
+    //update was successful, false otherwise. 
+
+    public boolean upadteArtist(int ArtistId) throws SQLException {
+        PreparedStatement ps = null;
+        PreparedStatement qs = null;
+        String sql = "UPDATE Artist SET Name = (?) WHERE ArtistId = (?)";
+        
+
+    }
+}
