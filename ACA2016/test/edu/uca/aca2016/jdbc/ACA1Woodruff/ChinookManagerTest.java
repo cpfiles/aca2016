49af969ac3323a99eb17657804dc9b8e17c964eb
awood <awood@LAPTOP-RACFKBC7.philander.edu>
2016-11-29 20:16:12 -0600

    Merge master into ACA1Woodruff004
    Conflicts:
    	ACA2016/src/edu/uca/aca2016/stereo/ACA1Woodruff/MyStereo.java
    	ACA2016/src/edu/uca/aca2016/stereo/cpfiles/MyStereo.java

--- ACA2016/test/edu/uca/aca2016/jdbc/ACA1Woodruff/ChinookManagerTest.java
+++ ACA2016/test/edu/uca/aca2016/jdbc/ACA1Woodruff/ChinookManagerTest.java
@@ -5,18 +5,6 @@
  */
 package edu.uca.aca2016.jdbc.ACA1Woodruff;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Properties;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -29,50 +17,12 @@ import static org.junit.Assert.*;
  * @author awood
  */
 public class ChinookManagerTest {
-    ChinookManager instance;
-    private static FileInputStream in;
-    edu.uca.aca2016.jdbc.ACA1Woodruff.ChinookManager instance;
-   
-    public ChinookManagerTest(){
+    
+    public ChinookManagerTest() {
     }
     
     @BeforeClass
-    public static void setUpClass() throws IOException, SQLException{
-        Connection con = null;
-        PreparedStatement ps = null;
-        
-        try{
-            Path inpath = Paths.get("resources","config","ACA1Woodruff","ChinookManager.properties");
-            FileInputStream in = new FileInputStream(inpath.toFile());
-            Properties Chinook = new Properties();
-            Chinook.load(in);
-            in.close();
-            
-            con = DriverManager.getConnection(Chinook.getProperty("db.connection"));
-            
-            ps = con.prepareStatement("DELETE FROM Artist");
-            ps.executeUpdate();
-            
-            ps = con.prepareStatement("INSERT INTO Artist(id, name) VALUES(276, 'Fleetwood Mac')");
-            ps.executeUpdate();
-        }
-        catch(FileNotFoundException ex){
-            
-        }
-        catch(IOException ex){
-            
-        }
-        catch(SQLException ex){
-            
-        }
-        finally {
-            if (ps != null) {
-                ps.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public static void setUpClass() {
     }
     
     @AfterClass
@@ -81,57 +31,69 @@ public class ChinookManagerTest {
     
     @Before
     public void setUp() {
-        this.instance = new edu.uca.aca2016.jdbc."ACA1Woodruff".ChinookManager();
-    }    
-//        try {
-//            this.instance = new ChinookManager();
-//
-//        } catch (IOException ex) {
-//            Logger.getLogger(ChinookManagerTest.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ChinookManagerTest.class.getName()).log(Level.SEVERE, null, ex);
-//        }*/
-    
+    }
     
     @After
     public void tearDown() {
     }
 
-//    @Test
-//    public void testSomeMethod() {
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//        
+    /**
+     * Test of addArtist method, of class ChinookManager.
+     */
     @Test
-    public void testAddArtist() throws SQLException{        
-        this.instance.addArtist("Fleetwood Mac");
+    public void testAddArtist() throws Exception {
+        System.out.println("addArtist");
+        String artist_name = "Fleetwood Mac";
+        ChinookManager instance = new ChinookManager();
+        instance.addArtist(artist_name);
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
     }
 
     /**
      * Test of getArtist method, of class ChinookManager.
      */
     @Test
-    public void testGetArtist() throws SQLException{
-        int result = instance.getArtist("Fleetwood Mac");
-        assertEquals(1, result);
+    public void testGetArtist() throws Exception {
+        System.out.println("getArtist");
+        String artist_name = "Fleetwood Mac";
+        ChinookManager instance = new ChinookManager();
+        int expResult = 276;
+        int x = instance.getArtist("Fleetwood Mac");
+        assertEquals(276, x);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
     }
 
     /**
      * Test of updateArtist method, of class ChinookManager.
      */
-    @Test
-    public void testUpdateArtist(){
-        boolean result = instance.updateArtist(276, "Fleetwood Mack");
-        assertEquals(true,result);
-    }
-
-    /**
-     * Test of deleteArtist method, of class ChinookManager.
-     */
-    @Test
-    public void testDeleteArtist(){
-        boolean result = instance.deleteArtist(276);
-        assertEquals(true,result);
-    }
-} 
-
+//    @Test
+//    public void testUpdateArtist() throws Exception {
+//        System.out.println("updateArtist");
+//        int Id = 276;
+//        String Name = "Fleetwood Mac";
+//        ChinookManager instance = new ChinookManager();
+//        boolean expResult = false;
+//        boolean result = instance.updateArtist(276, "Fleetwood Mac");
+//        assertEquals(expResult, result);
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of deleteArtist method, of class ChinookManager.
+//     */
+//    @Test
+//    public void testDeleteArtist() throws Exception {
+//        System.out.println("deleteArtist");
+//        int Id = 275;
+//        ChinookManager instance = new ChinookManager();
+//        boolean expResult = false;
+//        boolean result = instance.deleteArtist(276);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+}
