9ce10342e11d2f68a74310c8560b25f7932137c0
sethdumas <sethdumas@gmail.com>
2016-12-07 18:53:28 -0600

    006 ChinGenreManager 1

---
+++ ACA2016/src/edu/uca/aca2016/chinook/sethdumas/ChinookGenreManager.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.chinook.sethdumas;
+
+import java.sql.Connection;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sethd
+ */
+public class ChinookGenreManager {
+     Connection con = null;
+    private static final Logger logger = Logger.getLogger(ChinookGenreManager.class.getName());
+    
+    
+}
