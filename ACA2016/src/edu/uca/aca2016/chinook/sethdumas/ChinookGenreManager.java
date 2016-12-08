cb2bdda36e543fb6c3a7648e5c2dbc3fb00958fc
sethdumas <sethdumas@gmail.com>
2016-12-07 19:14:37 -0600

    006 ChinGenreManager 1

--- ACA2016/src/edu/uca/aca2016/chinook/sethdumas/ChinookGenreManager.java
+++ ACA2016/src/edu/uca/aca2016/chinook/sethdumas/ChinookGenreManager.java
@@ -5,16 +5,55 @@
  */
 package edu.uca.aca2016.chinook.sethdumas;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 /**
  *
  * @author sethd
  */
-public class ChinookGenreManager {
-     Connection con = null;
+public class ChinookGenreManager{
+    Connection con = null;
     private static final Logger logger = Logger.getLogger(ChinookGenreManager.class.getName());
     
-    
+    public ChinookGenreManager(){
+        try{
+            // attempt to read a "known" properties file that is on the classpath
+            Enumeration<URL> url = ChinookGenreManager.class.getClassLoader().getResources("config/sethdumas/ChinookManager.properties");
+            InputStream stream = new FileInputStream(url.nextElement().getPath());
+            Properties props = new Properties();
+            props.load(stream);
+            stream.close();
+            
+            logger.info("Connecting to database: " + props.getProperty("db.connection"));
+            
+            // the properties file loaded, attempt to connect using the value of the "db.connection" property
+            Class.forName("org.sqlite.JDBC");
+            if (this.con == null) {
+                con = DriverManager.getConnection(props.getProperty("db.connection"));
+            }
+        }
+        catch(FileNotFoundException ex){
+            logger.severe("File Not Found: " + ex.getMessage());
+        }
+        catch(IOException ex){
+            logger.severe("IO Exception: " + ex.getMessage());
+        }
+        catch(SQLException ex){
+            logger.severe("SQL Issue: " + ex.getMessage());
+        }
+        catch(ClassNotFoundException ex){
+            logger.severe("Class not found: " + ex.getMessage());
+        }
+    }
 }
+    
