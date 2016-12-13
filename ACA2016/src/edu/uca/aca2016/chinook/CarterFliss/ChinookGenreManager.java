2865a3fb90a4146f80fb23ef27ac0b9110090361
Carter Fliss <fcf007@msn.com>
2016-12-12 19:27:03 -0600

    added rest of methods to ChinookGenreManager.java

--- ACA2016/src/edu/uca/aca2016/chinook/CarterFliss/ChinookGenreManager.java
+++ ACA2016/src/edu/uca/aca2016/chinook/CarterFliss/ChinookGenreManager.java
@@ -15,6 +15,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -25,6 +27,7 @@ import java.util.logging.Logger;
 public class ChinookGenreManager {
     Connection con;
     private static final Logger logger = Logger.getLogger(ChinookGenreManager.class.getName());
+    PreparedStatement ps;
     
     public ChinookGenreManager(){
         try{
@@ -49,5 +52,96 @@ public class ChinookGenreManager {
         catch(SQLException ex){
             logger.severe("SQL Issue: " + ex.getMessage());
         }
+    }
+    
+    public HashMap<Integer,String> getGenres(){
+        HashMap<Integer,String> m = new HashMap<Integer,String>();
+        try{
+            this.ps = this.con.prepareStatement("SELECT * FROM Genre");
+            ResultSet rs = this.ps.executeQuery();
+            int genIDCol = rs.findColumn("GenreId");
+            int genNameCol = rs.findColumn("Name");
+            while(rs.next()){
+                int getGenID = rs.getInt(genIDCol);
+                String getGenName = rs.getString(genNameCol);
+                m.put(getGenID,getGenName);
+            }
+            
+        }catch(SQLException e){
+            logger.severe(e.getMessage());
+        }
+        return m;
+    }
+    
+    public boolean addGenre(String inName){
+        boolean tf = false;
+        try{
+            this.ps = this.con.prepareStatement("INSERT INTO Genre (Name) VALUES (?)");
+            this.ps.setString(1, inName);
+            int ga = this.ps.executeUpdate();
+            if (ga == 1){
+                tf = true;
+                logger.info(inName + " was added to database.");
+            }else{
+                logger.warning("Error entering "+inName+" into database.");
+            }
+            
+        }catch(SQLException e){
+            logger.severe(e.getMessage());
+        }
+        return tf;
+    }
+    
+    public String getGenreName (int IDpar){
+        String newGenre = null;
+        try{
+            this.ps = this.con.prepareStatement("SELECT * FROM Genre WHERE GenreId = (?)");
+            this.ps.setInt(1, IDpar);
+            ResultSet rs = this.ps.executeQuery();
+            if(rs.next()){
+                newGenre = rs.getString(1);
+                logger.info(rs.getString(1)+" was found.");
+            }
+        }catch (SQLException e){
+            logger.severe(e.getMessage());
+        }
+        return newGenre;
+    }
+    
+    public boolean updateGenre (int IDpar, String namePar){
+        boolean tf = false;
+        try{
+            this.ps = this.con.prepareStatement("UPDATE Genre SET Name = (?) WHERE GenreId = (?)");
+            this.ps.setString(1, namePar);
+            this.ps.setInt(2, IDpar);
+            int ga = this.ps.executeUpdate();
+            if (ga == 1){
+               tf = true;
+               logger.info("Genre updated entry " + IDpar + " to "+namePar);
+            }else{
+                logger.warning("Genre was NOT updated.");
+            }
+        }catch (SQLException e){
+            logger.severe(e.getMessage());
+        }
+        return tf;
+    }
+    
+    public boolean deleteGenre (int IDpar){
+        boolean tf = false;
+        try{
+            this.ps = this.con.prepareStatement("DELETE FROM Genre WHERE GenreId = (?)");
+            this.ps.setInt(1, IDpar);
+            int ga = this.ps.executeUpdate();
+            if (ga == 1){
+                tf = true;
+                logger.info("Genre w/ ID "+IDpar+" was deleted.");
+            }else{
+                logger.warning("Genre was NOT deleted.");
+            }
+        }catch (SQLException e){
+            logger.severe(e.getMessage());
+        }
+        return tf;
     }
 }
