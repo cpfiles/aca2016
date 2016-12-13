e8528489ae0af345275b308bccb712ffd8d667b9
Username <Username@10.108.1.90>
2016-12-12 20:15:03 -0600

    save

--- ACA2016/src/edu/uca/aca2016/chinook/clintlemons/ChinookGenreManager.java
+++ ACA2016/src/edu/uca/aca2016/chinook/clintlemons/ChinookGenreManager.java
@@ -11,11 +11,14 @@ import java.io.*;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-
-
 
 /**
  *
@@ -52,6 +55,122 @@ public class ChinookGenreManager {
         }
         catch(ClassNotFoundException ex){
             logger.severe("Class not found: " + ex.getMessage());
+        }   
+    }
+
+    /**
+     *
+     * @param GenreID
+     * @param Name
+     * @return GenreID and Name 
+     */
+    public HashMap<Integer, String> getGenres(int GenreID, String Name){
+        
+        HashMap<Integer, String> Genres = new HashMap<>();
+        try{
+            Statement s = this.con.createStatement();
+            
+            ResultSet rs = s.executeQuery("SELECT * FROM Genre");
+            
+            while (rs.next()) {
+                Genres.put(rs.getInt("GenreID"), rs.getString("Name"));
+            }
         }
+        catch(SQLException ex){
+            logger.log(Level.SEVERE, "SQL Issue: {0}", ex.getMessage());
+        }
+        return Genres;
+    }
+        public boolean addGenre(String name) {
+            boolean ret = false;
+        try{
+            PreparedStatement ps = this.con.prepareStatement("INSERT INTO Genre (Name) VALUES(?)");
+            ps.setString(1,name);
+            int ra = ps.executeUpdate();
+            if (ra == 1) {
+                ret = true;
+                logger.info("Added genre '" + name + "' to the database ");
+                ps.close();
+            }
+            else{
+                ps.close();
+            }
+            
+        }
+        catch(SQLException ex){
+            logger.severe("Issue updating genre: " + ex.getMessage());
+        } 
+        return ret;
+    }
+        public String getGenreName(int id) {
+        String ret = null;
+
+        try{
+            // attempt to find the genre by id
+            PreparedStatement ps = this.con.prepareStatement("SELECT * FROM Genre WHERE GenreId = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            
+            // if there is a record, get the genre id to return
+            if (rs.next()) {
+                ret = rs.getString("Name");
+                logger.info("Search by id for genre '" + id + "' yielded an name of " + ret);
+            }
+            else {
+                logger.info("Search for genre '" + id + "' yielded no results");
+            }
+        }
+        catch(SQLException ex){
+            logger.severe("Issue searching for genre: " + ex.getMessage());
+        }
+
+        // send back null or the name
+        return ret;
+    }
+        public boolean updateGenre(int id, String name) {
+        boolean ret = false;
+        
+        try{
+            PreparedStatement ps = this.con.prepareStatement("UPDATE Genre SET Name = ? WHERE GenreId = ?");
+            ps.setString(1, name);
+            ps.setInt(2, id);
+            int ra = ps.executeUpdate();
+            
+            if (ra == 1) {
+                ret = true;
+                logger.info("Updated genre with ID " + id + " set name to '" + name + "'");
+            }
+            else {
+                logger.warning("Update genre with ID " + id + " had an undesired result and changed " + ra + " records");
+            }
+        }
+        catch(SQLException ex){
+            logger.severe("Issue updating genre: " + ex.getMessage());
+        }
+        
+        return ret;
+    }
+            
+        public boolean deleteGenre(int id) {
+        boolean ret = false;
+        
+        try{
+            PreparedStatement ps = this.con.prepareStatement("DELETE FROM Genre WHERE GenreId = ?");
+            ps.setInt(1, id);
+            int ra = ps.executeUpdate();
+            
+            if (ra == 1) {
+                ret = true;
+                logger.info("Deleted genre with ID " + id);
+            }
+            else {
+                logger.warning("Delete genre with ID " + id + " had an undesired result and changed " + ra + " records");
+            }
+        }
+        catch(SQLException ex){
+            logger.severe("Issue deleting genre: " + ex.getMessage());
+        }
+        
+        return ret;
     }
 }
