b154ac134169e5d7186043963e3ba292114a0802
Carter Fliss <fcf007@msn.com>
2016-12-13 18:44:29 -0600

    added runner class to test.

---
+++ ACA2016/src/edu/uca/aca2016/chinook/CarterFliss/GenreRunner.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.uca.aca2016.chinook.CarterFliss;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Carter
+ */
+public class GenreRunner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ChinookGenreManager gm = new ChinookGenreManager();
+        HashMap <Integer, String> x = gm.getGenres();
+        for(String y : x.values()){
+            System.out.println(y);
+        }
+//        gm.addGenre("Thrash Metal");
+//        gm.getGenreName(26);
+//        gm.updateGenre(26, "Death Metal");
+//        gm.deleteGenre(26);
+    }
+    
+}
