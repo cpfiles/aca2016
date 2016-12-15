331676ac22f60b859d4c62f675ab536a3241a010
xulix <xulix@10.6.0.221>
2016-12-15 12:59:23 -0600

    Genre Manager commit

--- ChinookManager/src/java/edu/uca/aca2016/web/curiologic/GenreManager.java
+++ ChinookManager/src/java/edu/uca/aca2016/web/curiologic/GenreManager.java
@@ -35,22 +35,7 @@ public class GenreManager extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-//    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-//            throws ServletException, IOException {
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet GenreManager</title>");            
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet GenreManager at " + request.getContextPath() + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
-//    }
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -109,7 +94,7 @@ public class GenreManager extends HttpServlet {
         }
     }
 
-    // processRequest(request, response);
+   
     /**
      * Handles the HTTP <code>POST</code> method.
      *
@@ -180,5 +165,5 @@ public class GenreManager extends HttpServlet {
     }
 }
 
-//processRequest(request, response);
+
 
